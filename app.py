d7d8bca804660a9d957b6955acdbd833a1f06513
vyshnaradh06 <vyshnaradh06@gmail.com>
2026-05-14 12:21:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Welcome to My Website</h1>
-    <p>This website is running with Python Flask.</p>
-    """
+    return "<h1>Welcome to My Website</h1>"
 
 @app.route("/about")
 def about():
-    return """
-    <h2>About Page</h2>
-    <p>This is a simple Flask web application.</p>
-    """
+    return "<h2>About Page</h2>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
